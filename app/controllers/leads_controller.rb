22b678cc0ea49902f9516929344bf80b00ed4cc2
Matheus Lucena <matheuslucena@gmail.com>
2015-08-11 12:54:54 -0300

    Modifying code to test the lead_create GEM

--- app/controllers/leads_controller.rb
+++ app/controllers/leads_controller.rb
@@ -1,26 +1,12 @@
 class LeadsController < ApplicationController
-  include Databasedotcom::Rails::Controller
 
   def index
-    @leads = Lead.all
+    @leads = LeadCreate.list_leads
   end
 
   def new
     contato = Contato.find(params[:contato_id])
-    user = User.first
-    @lead = Lead.new
-    @lead.OwnerId = user.Id
-    @lead.FirstName = contato.name
-    @lead.LastName = contato.last_name
-    @lead.Email = contato.email
-    @lead.Company = contato.company
-    @lead.Title = contato.job_title
-    @lead.Phone = contato.phone
-    @lead.Website = contato.website
-    @lead.IsConverted = false
-    @lead.IsUnreadByOwner = true
-    if @lead.save
-      redirect_to leads_path, :notice => contato.name + ' successfully added'
-    end
+    message = LeadCreate.create(contato)
+    redirect_to leads_path, :notice => message
   end
 end
